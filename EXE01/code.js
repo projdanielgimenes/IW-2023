df338eca242e4d13e4a3457d87b83ea28daad6d1
Daniel Gimenes <100932065+projdanielgimenes@users.noreply.github.com>
2023-03-07 11:58:45 -0300

    Add files via upload

---
+++ EXE01/code.js
@@ -0,0 +1,21 @@
+function troca0(){
+    window.location.href = "index.html"
+
+}
+
+function troca(){
+
+    window.location.href = "pagina2.html"
+}
+
+function troca1(){
+    window.location.href = "pagina3.html"
+}
+
+function troca2(){
+    window.location.href = "pagina4.html"
+}
+
+function troca3(){
+    window.location.href = "pagina5.html"
+}
