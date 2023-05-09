4711caa53fa8878cb03fc38e15385cd7c188285c
Daniel Gimenes <100932065+projdanielgimenes@users.noreply.github.com>
2023-05-09 10:58:36 -0300

    Add files via upload

---
+++ exemplo_01/teste.js
@@ -0,0 +1,27 @@
+//var nome = prompt("Digite seu nome: ")
+//console.log(nome)
+//alert("Bem Vindo " + nome)
+
+/*
+var a = 15
+if (true){
+const a = 10
+a = 20
+}
+console.log(a)
+*/
+
+// Calculando a soma de 2 números
+
+var n1 =parseInt (prompt("Digite N1: "))
+var n2 =parseInt (prompt("Digite N2: "))
+
+var soma = n1 + n2
+
+//console.log("A soma é " + soma)
+
+//Template String
+//console.log(`A soma é ${soma}`)
+document.write(`<p class="soma">A soma é ${soma}</p>`)
+
+//console.log(typeof n1)
